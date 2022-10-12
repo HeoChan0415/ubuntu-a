353681db6ca109ee1efa5faa39cfe0d037028a3c
HeoChan0415 <113495681+HeoChan0415@users.noreply.github.com>
2022-10-12 15:39:35 +0900

    1234

--- shells/if.sh
+++ shells/if.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-man=10
-woman=20
-if [ $man -lt $woman ]
-then
-		echo woman
+man=0
+woman=0
+echo -n " man age : "
+read man
+echo -n " woman age : "
+read woman
+
+if [ $man -gt $woman ]; then
+		echo man : $man
+else
+		echo woman : $woman
 fi
