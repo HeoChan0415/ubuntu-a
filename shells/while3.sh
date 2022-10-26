ccba0f8dd2778f31cbca90b936727613f072ea40
HeoChan0415 <113495681+HeoChan0415@users.noreply.github.com>
2022-10-26 15:24:23 +0900

    1

--- shells/while3.sh
+++ shells/while3.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-
-
+a=1
 row="$1"
 while [[ 10 -gt $row ]]; do
+	if [ $a -lt 1 -o $a -gt 9 ]; then
+		echo "This program is require number 1~9."
+	else
+		for ((k=1;k<=9;k++))do
+        echo "$a - $k = `expr $a \- $k`"
 	echo "$row"
 	row=`echo "$row"+1|bc`
+done
+fi
 done
