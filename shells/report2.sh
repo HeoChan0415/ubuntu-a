b90b28594cb975d6d8248036ea2d493acd86d646
HeoChan0415 <113495681+HeoChan0415@users.noreply.github.com>
2022-11-02 16:15:36 +0900

    123

--- shells/report2.sh
+++ shells/report2.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-set -- $(getopt -q ab:cd "$@")
-while [ -n $2 ]; do
-  case $2 in
-    -+) echo "[ $1 + $3 ]" ;;
-    --) echo "[ $1 - $3 ]" ;;
-    -X) echo "[ $1 * $3 ]" ;;
-    -/) echo "[ $1 / $3 ]" ;;
-    -~) echo "[ second prameter is only '+ = X /' ]" ;;
-      *) echo "second parameter is only '+=X/'" ;;
-    esac
-  done
+if [ $# -ne 3 ]; then
+	echo "This program require theree paramter"
+elif [ $# -eq 3 ]; then
+	case "$2" in
+	+)	let z=$1+$3
+      echo $1 + $3 = $z;;
+	-)	let z=$1-$3
+      echo $1 - $3 = $z;;
+	X)	let z=$1*$3
+      echo $1 X $3 = $z;;
+	/)	let z=$1/$3
+      echo $1 / $3 = $z;;
+	*)	echo "second prameter is only '+ = X /'"
+	esac
+fi
